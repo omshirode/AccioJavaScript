30081d3c7bfb47725ea7bff34cb22788431eeaf8
unknown <omshirode007@gmail.com>
2025-09-17 00:27:14 +0530

    String Searching

---
+++ Strings/STRsearching.js
@@ -0,0 +1,6 @@
+let str="Hey I am Learning JavaScript";
+//Searching
+console.log(str.indexOf("I"));
+console.log(str.includes("am"));
+console.log(str.startsWith("I"));
+console.log(str.endsWith("JavaScript"));
