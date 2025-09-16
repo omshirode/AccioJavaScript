dcc5f4575d0cfdf978d0f272db8c10c4f340a887
unknown <omshirode007@gmail.com>
2025-09-17 00:21:28 +0530

    String Manipulation

---
+++ Strings/STRmanipulation.js
@@ -0,0 +1,17 @@
+let string="Assembly";
+
+//string upper and lower
+console.log(string.toUpperCase());
+console.log(string.toLowerCase());
+
+//Trimming
+let trim=" Hello ";
+console.log(trim.trim());
+console.log(trim.trimEnd());
+console.log(trim.trimStart());
+
+//split and join
+let thing="Om,Shirode";
+let arr=thing.split(",");
+console.log(arr);
+console.log(arr.join(" | "));
